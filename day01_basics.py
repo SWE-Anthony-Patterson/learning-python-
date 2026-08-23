581c10d5297b6bd0774c291787f3d2503e6a01d0
Anthony Patterson <Patterson4840@yahoo.com>
2026-08-23 21:43:12 +0000

    Complete Python basics refresher

--- day01_basics.py
+++ day01_basics.py
@@ -1,8 +1,20 @@
 #Relearning Python Basics
-name = "Anthony"
-age = 30
+age = int(input("How old are you? "))
 favorite_food = "spicy chicken"
+city = "Dallas"
+car = "Honda"
+year = 2024
+a = "10"
+b = "5"
+x = "5"
+y = "3"
+name = input("What is your name? ")
 
 print(name)
-print(age)
+print(age+5)
 print(favorite_food)
+print(city)
+print(car)
+print(int(a) + int(b))
+print(year)
+print(x + y)
